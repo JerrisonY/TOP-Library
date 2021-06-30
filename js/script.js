be59a83fdc1c916bdf19bb9eddf6d0965611e24b
Jerrison <jerryyip24@gmail.com>
2021-06-30 17:50:45 -0400

    Created Add book button container layout

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const addBook = document.querySelector('#addBook')
+
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
@@ -12,9 +14,16 @@ let harryPotter = new Book('Harry Potter', 'J.K. Rowling', '542', 'has been read
 console.log(harryPotter.info())
 
 
-let myLibrary = [];
+let myLibrary = [
+    {name: 'The City of Bone', author: 'Cassandra Clare', pages: 550, read: 'Read'},
+];
 
-let addBookToLibrary = (book) => {
+let addBookToLibrary = () => {
     
 }
+ console.log(1+1)
 
+ addBook.addEventListener('click', () => {
+    prompt('Name of book')
+ })
+ 
