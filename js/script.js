9ec6420d3aed729eced1dd5f33e1b22e263de6c3
Jerrison <jerryyip24@gmail.com>
2021-07-03 01:38:39 -0400

    Added JS DOM support for card creation

--- js/script.js
+++ js/script.js
@@ -1,25 +1,63 @@
 const libraryContainer = document.querySelector('.library-container')
+const cardContainer = document.querySelector('.card-container')
 const newBookWindow = document.querySelector('.new-book-container')
 const newBookBtn = document.querySelector('#addBook')
 const closeWindow = document.querySelector('.close-book-form')
 
-function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-    this.info = () => {
-        return `${title} by ${author}, ${pages} pages, ${read}.`
+
+let myLibrary = [
+    {name: 'The City of Bone', author: 'Cassandra Clare', pages: 550, read: 'Read', genre: 'fantasy'},
+    {name: 'Harry Potter', author: 'J.K. Rowling', pages: 780, read: 'Read', genre: 'fantasy'},
+    {name: 'Gatsby', author: 'Fitzgerald', pages: 450, read: 'Read', genre: 'fantasy'},
+    {name: 'Percy Jackson', author: 'Coleman S.', pages: 250, read: 'Not read', genre: 'fantasy'},
+];
+
+let generateCards = () => {
+    for (let i = 0; i < myLibrary.length; i++) {
+        let newCard = document.createElement('div');
+        let cardTitle = document.createElement('h3');
+        let cardAuthor = document.createElement('p');
+        let cardPages = document.createElement('p');
+        let cardGenre = document.createElement('p');
+        let cardRead = document.createElement('p');
+        let cardExit = document.createElement('span')
+
+        newCard.classList.add('new-card')
+        cardTitle.textContent = `${myLibrary[i].name}`
+        cardAuthor.textContent = `By: ${myLibrary[i].author}`
+        cardPages.textContent = `Pages: ${myLibrary[i].pages}`
+        cardGenre.textContent = `Genre: ${myLibrary[i].genre}`
+        cardRead.textContent = `${myLibrary[i].read}`
+        cardExit.textContent = 'x'
+        cardContainer.appendChild(newCard)
+        newCard.append(cardTitle, cardAuthor, cardPages, cardGenre, cardRead, cardExit)
+        
+        // determines what color the card is depending on read status
+        cardRead.textContent === 'Read'
+            ? newCard.style.backgroundColor = '#DAF7A6'
+            : newCard.style.backgroundColor = '#FFC300'
+
+        cardRead.style.cursor = 'pointer'
+        
+        // on click color change for read status
+        cardRead.addEventListener('click', () => {
+            if (cardRead.textContent === 'Read') {
+                cardRead.textContent = 'Not read'
+                newCard.style.backgroundColor = '#FFC300'
+            } else {
+                cardRead.textContent = 'Read'
+                newCard.style.backgroundColor = '#DAF7A6'
+            }
+        })
+
+        // deletes card
+        cardExit.addEventListener('click', () => {
+            newCard.remove()
+        })
     }
 }
 
-let harryPotter = new Book('Harry Potter', 'J.K. Rowling', '542', 'has been read')
-console.log(harryPotter.info())
-
-
-let myLibrary = [
-    {name: 'The City of Bone', author: 'Cassandra Clare', pages: 550, read: 'Read'},
-];
+generateCards();
 
 let addBookToLibrary = () => {
     
@@ -34,3 +72,5 @@ newBookBtn.addEventListener('click', () => {
      newBookWindow.classList.toggle('display-none')
      libraryContainer.classList.toggle('background-blur')
  })
+
+ // create more objects, find way to display array as cards (function)
