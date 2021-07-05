110c92616f107fd356db56bcebca8f857c150c5d
Jerrison <jerryyip24@gmail.com>
2021-07-05 00:28:28 -0400

    Finished 1st draft of UI (Have to add new book function and then lightmode)

--- js/script.js
+++ js/script.js
@@ -6,10 +6,22 @@ const closeWindow = document.querySelector('.close-book-form')
 
 
 let myLibrary = [
-    {name: 'The City of Bone', author: 'Cassandra Clare', pages: 550, read: 'Read', genre: 'fantasy'},
-    {name: 'Harry Potter', author: 'J.K. Rowling', pages: 780, read: 'Read', genre: 'fantasy'},
-    {name: 'Gatsby', author: 'Fitzgerald', pages: 450, read: 'Read', genre: 'fantasy'},
-    {name: 'Percy Jackson', author: 'Coleman S.', pages: 250, read: 'Not read', genre: 'fantasy'},
+    {name: 'The City of Bone', author: 'Cassandra Clare', pages: 550, read: 'Read', genre: 'Fantasy'},
+    {name: 'Harry Potter', author: 'J.K. Rowling', pages: 780, read: 'Read', genre: 'Fantasy'},
+    {name: 'The Great Gatsby', author: 'Fitzgerald', pages: 450, read: 'Read', genre: 'Historical Fiction'},
+    {name: 'Percy Jackson and the Olympians', author: 'Coleman S.', pages: 250, read: 'Not read', genre: 'Fantasy'},
+    {name: 'Percy Jackson and the Olympians', author: 'Coleman S.', pages: 250, read: 'Not read', genre: 'Fantasy'},
+    {name: 'Percy Jackson and the Olympians', author: 'Coleman S.', pages: 250, read: 'Not read', genre: 'Fantasy'},
+    {name: 'Percy Jackson and the Olympians', author: 'Coleman S.', pages: 250, read: 'Not read', genre: 'Fantasy'},
+    {name: 'Percy Jackson and the Olympians', author: 'Coleman S.', pages: 250, read: 'Not read', genre: 'Fantasy'},
+    {name: 'Percy Jackson and the Olympians', author: 'Coleman S.', pages: 250, read: 'Not read', genre: 'Fantasy'},
+    {name: 'Percy Jackson and the Olympians', author: 'Coleman S.', pages: 250, read: 'Not read', genre: 'Fantasy'},
+    {name: 'Percy Jackson and the Olympians', author: 'Coleman S.', pages: 250, read: 'Not read', genre: 'Fantasy'},
+    {name: 'Percy Jackson and the Olympians', author: 'Coleman S.', pages: 250, read: 'Not read', genre: 'Fantasy'},
+    {name: 'Percy Jackson and the Olympians', author: 'Coleman S.', pages: 250, read: 'Not read', genre: 'Fantasy'},
+    {name: 'Percy Jackson and the Olympians', author: 'Coleman S.', pages: 250, read: 'Not read', genre: 'Fantasy'},
+    {name: 'Percy Jackson and the Olympians', author: 'Coleman S.', pages: 250, read: 'Not read', genre: 'Fantasy'},
+    {name: 'Percy Jackson and the Olympians', author: 'Coleman S.', pages: 250, read: 'Not read', genre: 'Fantasy'},
 ];
 
 let generateCards = () => {
@@ -19,10 +31,14 @@ let generateCards = () => {
         let cardAuthor = document.createElement('p');
         let cardPages = document.createElement('p');
         let cardGenre = document.createElement('p');
-        let cardRead = document.createElement('p');
-        let cardExit = document.createElement('span')
+        let cardRead = document.createElement('span');
+        let cardExit = document.createElement('span');
 
         newCard.classList.add('new-card')
+        cardRead.classList.add('read-container')
+        cardExit.classList.add('new-card-exit')
+        cardGenre.style.paddingBottom = '40px'
+
         cardTitle.textContent = `${myLibrary[i].name}`
         cardAuthor.textContent = `By: ${myLibrary[i].author}`
         cardPages.textContent = `Pages: ${myLibrary[i].pages}`
@@ -33,20 +49,28 @@ let generateCards = () => {
         newCard.append(cardTitle, cardAuthor, cardPages, cardGenre, cardRead, cardExit)
         
         // determines what color the card is depending on read status
-        cardRead.textContent === 'Read'
-            ? newCard.style.backgroundColor = '#DAF7A6'
-            : newCard.style.backgroundColor = '#FFC300'
+        if (cardRead.textContent === 'Read') {
+            cardRead.classList.add('read-span')
+            newCard.classList.add('read-div')
+        } else {
+            cardRead.classList.add('not-read-span')
+            newCard.classList.add('not-read-div')
+        }
 
-        cardRead.style.cursor = 'pointer'
-        
         // on click color change for read status
         cardRead.addEventListener('click', () => {
             if (cardRead.textContent === 'Read') {
                 cardRead.textContent = 'Not read'
-                newCard.style.backgroundColor = '#FFC300'
+                cardRead.classList.toggle('not-read-span') 
+                cardRead.classList.toggle('read-span') 
+                newCard.classList.toggle('not-read-div')
+                newCard.classList.toggle('read-div')
             } else {
                 cardRead.textContent = 'Read'
-                newCard.style.backgroundColor = '#DAF7A6'
+                cardRead.classList.toggle('not-read-span')
+                cardRead.classList.toggle('read-span')
+                newCard.classList.toggle('not-read-div')
+                newCard.classList.toggle('read-div')
             }
         })
 
@@ -68,9 +92,9 @@ newBookBtn.addEventListener('click', () => {
     libraryContainer.classList.toggle('background-blur')
 })
 
- closeWindow.addEventListener('click', () => {
-     newBookWindow.classList.toggle('display-none')
-     libraryContainer.classList.toggle('background-blur')
- })
+closeWindow.addEventListener('click', () => {
+    newBookWindow.classList.toggle('display-none')
+    libraryContainer.classList.toggle('background-blur')
+})
 
  // create more objects, find way to display array as cards (function)
